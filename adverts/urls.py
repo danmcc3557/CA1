d1a276885fd8d4ad9ba42272f77336bffc1fea86
Daniel McCarthy <=>
2021-11-16 14:42:40 +0000

    password change added

--- adverts/urls.py
+++ adverts/urls.py
@@ -9,9 +9,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('<uuid:pk>/edit/', AdvertUpdateView.as_view(), name='advert_edit'),
-    path('<uuid:pk>', AdvertDetailView.as_view(), name='advert_detail'),
-    path('<uuid:pk>/delete/', AdvertDeleteView.as_view(), name='advert_delete'),
+    path('<int:pk>/edit/', AdvertUpdateView.as_view(), name='advert_edit'),
+    path('<int:pk>', AdvertDetailView.as_view(), name='advert_detail'),
+    path('<int:pk>/delete/', AdvertDeleteView.as_view(), name='advert_delete'),
     path('/new/', AdvertCreateView.as_view(), name='advert_new'),
     path('', AdvertListView.as_view(), name='advert_list'),
 ]
